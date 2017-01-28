568ce7f88e42a9f11f035ff2f78f9247c7e090be
Ludovic Barman <ludovic.barman@gmail.com>
2017-01-28 12:09:42 +0100

    New webhook

--- .webhook.php
+++ .webhook.php
@@ -2,8 +2,6 @@
 $PATH = "/var/www/prifi-web";
 $SECRET = "nice_try";
 
-file_put_contents('filename.txt', print_r($_POST, true));
-
 shell_exec("cd $PATH; git reset --hard; git clean -fd");
 shell_exec("cd $PATH; git pull");
 ?>
